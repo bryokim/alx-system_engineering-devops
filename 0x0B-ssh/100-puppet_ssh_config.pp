6362d08508a7af69cc3a489e94d20b47b67d99bb
Brian Kim <bryo.kim1@gmail.com>
2023-06-25 20:58:04 +0300

    Initial commit for 0x0B-ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,21 @@
+# Makes changes to the ssh config file
+# IdentityFile => ~/.ssh/school
+# PasswordAuthentication => no
+
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  match   => '^[#]*[\s]*(?i)PasswordAuthentication[\s]+(yes|no)[\s]*$',
+  replace => true,
+}
+
+file_line { 'Declare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  match   => '^[#]*[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa[\s]*$',
+  replace => true,
+}
