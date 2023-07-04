e3db12bf223d4c4957ef8836fb3564cb708b9881
Brian Kim <bryo.kim1@gmail.com>
2023-07-04 20:23:39 +0300

    Config file being edited changed. Added rule for redirection.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,8 +2,6 @@
 
 include stdlib
 
-$line = '        add_header X-Served-By $hostname always;'
-
 exec { 'update system':
   provider => shell,
   command  => 'apt-get update -y',
@@ -16,11 +14,22 @@ Package { 'nginx':
   notify => Service['nginx']
 }
 
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => inline_template('<%= "Hello World!\n " %>'),
+  require => Package['nginx'],
+}
+
+exec {'redirect_me':
+  command  => 'sed -i "25i\ \trewrite ^/redirect_me https://www.youtube.com/v=QH2-TGUlwu4 permanent;\n" /etc/nginx/sites-available/default',
+  provider => 'shell'
+}
+
 file_line { 'add_header':
   ensure => present,
-  line   => $line,
-  after  => '^http {',
-  path   => '/etc/nginx/nginx.conf',
+  line   => inline_template('<%= "\n\tadd_header X-Served-By $hostname always;" %>'),
+  after  => '^(\s)+server_name (.)+$',
+  path   => '/etc/nginx/sites-available/default',
   notify => Service['nginx'],
 }
 
