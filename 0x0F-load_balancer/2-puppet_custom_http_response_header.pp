72b3522540b91241eb553c117a11f426217e8245
Brian Kim <bryo.kim1@gmail.com>
2023-07-04 20:35:50 +0300

    Removed dependency on stdlib

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,38 +1,31 @@
 # Creates custom HTTP response header "X-Served-By"
 
-include stdlib
-
 exec { 'update system':
-  provider => shell,
-  command  => 'apt-get update -y',
-  before   => Package['nginx'],
+  command => '/usr/bin/apt-get update',
 }
 
-Package { 'nginx':
-  ensure => installed,
-  before => File_line['add_header'],
-  notify => Service['nginx']
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update system']
 }
 
 file { '/var/www/html/index.html':
   ensure  => present,
-  content => inline_template('<%= "Hello World!\n " %>'),
-  require => Package['nginx'],
+  content => inline_template('<%= "Hello World!\n" %>'),
 }
 
-exec {'redirect_me':
-  command  => 'sed -i "25i\ \trewrite ^/redirect_me https://www.youtube.com/v=QH2-TGUlwu4 permanent;\n" /etc/nginx/sites-available/default',
-  provider => 'shell'
+exec { 'redirect_me':
+  command  => 'sed -i "24i\	\nrewrite ^/redirect_me https://www.youtube.com/v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell',
 }
 
-file_line { 'add_header':
-  ensure => present,
-  line   => inline_template('<%= "\n\tadd_header X-Served-By $hostname always;" %>'),
-  after  => '^(\s)+server_name (.)+$',
-  path   => '/etc/nginx/sites-available/default',
-  notify => Service['nginx'],
+exec { 'add_header':
+  command  => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+  provider => 'shell',
+  notify   => Service['nginx']
 }
 
 service { 'nginx':
-  ensure => running,
+  ensure  => running,
+  require => Package['nginx']
 }
